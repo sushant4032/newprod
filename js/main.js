3a6e350d6948bf0de93842f521dbbcf231e8b0a0
sushant4032 <sushant4032@gmail.com>
2020-04-02 10:42:44 +0530

    Date function implemented

--- js/main.js
+++ js/main.js
@@ -1,31 +1,24 @@
-// const dummyBtn = document.querySelector('#dummy');
-// dummyBtn.addEventListener('click', dummy);
-
 
 const pf = document.querySelectorAll('.production table input');
 const hf = document.querySelectorAll('.hemm table input');
 const of = document.querySelectorAll('.others table input');
 
+const df = document.querySelector('#date');
+
 const pfv = {};
 const hfv = {};
 const ofv = {};
 
-const pc = {};
-const hc = {};
-const oc = {};
+const pcv = {};
+const hcv = {};
+const ocv = {};
 
 
-
-
-const f = document.querySelectorAll('input');
-f.forEach(function (x) {
+const adf = document.querySelectorAll('input');
+adf.forEach(function (x) {
     x.addEventListener('change', calculate);
 });
 
-
-const pdf = document.querySelector('#pdate');
-const hdf = document.querySelector('#hdate');
-const odf = document.querySelector('#odate');
 
 start();
 
@@ -33,36 +26,50 @@ function start() {
     const a = new Date();
     const b = a.toISOString();
     const c = b.slice(0, 10);
-    console.log(c);
-    pdf.value = c; 
-
-    const n = date2number(pdf.value);
-    console.log(n);
+    df.value = c; 
+    updateDates();
 }
 
+function updateDates() {
+    const dt = df.value;
+    cdn = date2number(dt);
+    pdn = cdn - 1;
+    hdn = cdn - 2;
+    pdf = formatedDate(pdn);
+    hdf = formatedDate(hdn);
+    document.querySelector('.pdate-disp').innerHTML = pdf;
+    document.querySelector('.hdate-disp').innerHTML = hdf;
+    document.querySelector('.odate-disp').innerHTML = pdf;
 
+}
 
 function date2number(dt) {
-
-
-    const a = new Date(2020, 3, 1, 0, 6, 0, 0);
-    const b = new Date(a);
-    const c = a.getTime();
-
-
+    const a = new Date(2018, 3, 1, 0, 6, 0, 0);
+    const b = a.getTime();
     const u = new Date(dt);
-    
     const v = u.getTime();
-    console.log(v);
-    const w = Math.ceil((v - c) / (24 * 3600 * 1000))
+    const w = Math.ceil((v - b) / (24 * 3600 * 1000))
     return w;
+}
 
+function number2date(dn) {
+
+    const a = new Date(2018, 3, 1, 0, 6, 0, 0);
+    const b = a.getTime();
+    const c = b + (dn-1) * (24 * 3600 * 1000);
+    const d = new Date(c);
+    return d;
+}
+
+function formatedDate(dn) {
+    const a = new Date(number2date(dn));
+    return a.toString().slice(0, 15);
 }
 
 function calculate() {
 
-
-    date2number();
+    updateDates();
+ 
 
     pf.forEach(x => {
         pfv[x.id] = +x.value;
@@ -78,76 +85,48 @@ function calculate() {
     });
 
 
-    pc.coalsd = pfv.g8 + pfv.g10sd;
-    pc.coalsm = pfv.g10sm;
+    pcv.coalsd = pfv.g8 + pfv.g10sd;
+    pcv.coalsm = pfv.g10sm;
 
-    pc.totalcoal = pc.coalsd + pc.coalsm;
+    pcv.totalcoal = pcv.coalsd + pcv.coalsm;
 
-    pc.obsd = pfv.depteast + pfv.deptwest + pfv.deptcoal;
-    pc.obdl = pfv.jwalasolid + pfv.vindhyasolid + pfv.jyotisolid + pfv.pawansolid;
-    pc.dlreh = pfv.jwalareh + pfv.vindhyareh + pfv.jyotireh + pfv.pawanreh;
+    pcv.obsd = pfv.depteast + pfv.deptwest + pfv.deptcoal;
+    pcv.obdl = pfv.jwalasolid + pfv.vindhyasolid + pfv.jyotisolid + pfv.pawansolid;
+    pcv.dlreh = pfv.jwalareh + pfv.vindhyareh + pfv.jyotireh + pfv.pawanreh;
 
-    pc.deptob = pc.obsd + pc.obdl;
+    pcv.deptob = pcv.obsd + pcv.obdl;
+    pcv.outob = pfv.outeasttop + pfv.outwesttop + pfv.outeastmid + pfv.outwestmid + pfv.outeastdl + pfv.outwestdl;
+    pcv.totalob = pcv.deptob + pcv.outob;
 
-    pc.outob = pfv.outeasttop + pfv.outwesttop + pfv.outeastmid + pfv.outwestmid + pfv.outeastdl + pfv.outwestdl;
-
-    pc.totalob = pc.deptob + pc.outob;
-
-    pc.silog8 = pfv.g8;
-    pc.silog10 = pfv.totalsilo - pfv.g8;
-    pc.totaldesp = pfv.totalsilo + pfv.wwg10 + pfv.roadg10;
-    pc.netdesp = pc.totaldesp - pfv.rwwg10 - pfv.rchpg10;
+    pcv.silog8 = pfv.g8;
+    pcv.silog10 = pfv.totalsilo - pfv.g8;
+    pcv.totaldesp = pfv.totalsilo + pfv.wwg10 + pfv.roadg10;
+    pcv.netdesp = pcv.totaldesp - pfv.rwwg10 - pfv.rchpg10;
 
 
 
-    hc.dlah = hfv.dlavl;
-    hc.dlwh = hfv.dlutl;
-    hc.shvah = hfv.shvavl + hfv.shvutls;
-    hc.shvwh = hfv.shvutl + hfv.shvutls;
-    hc.d85ah = hfv.d85avl + hfv.d85utls;
-    hc.d85wh = hfv.d85utl + hfv.d85utls;
-    hc.d120ah = hfv.d120avl + hfv.d120utls;
-    hc.d120wh = hfv.d120utl + hfv.d120utls;
-    hc.dumperah = hfv.d85avl + hfv.d85utls + hfv.d120avl + hfv.d120utls;
-    hc.dumperwh = hfv.d85utl + hfv.d85utls + hfv.d120utl + hfv.d120utls
+    hcv.dlah = hfv.dlavl;
+    hcv.dlwh = hfv.dlutl;
+    hcv.shvah = hfv.shvavl + hfv.shvutls;
+    hcv.shvwh = hfv.shvutl + hfv.shvutls;
+    hcv.d85ah = hfv.d85avl + hfv.d85utls;
+    hcv.d85wh = hfv.d85utl + hfv.d85utls;
+    hcv.d120ah = hfv.d120avl + hfv.d120utls;
+    hcv.d120wh = hfv.d120utl + hfv.d120utls;
+    hcv.dumperah = hfv.d85avl + hfv.d85utls + hfv.d120avl + hfv.d120utls;
+    hcv.dumperwh = hfv.d85utl + hfv.d85utls + hfv.d120utl + hfv.d120utls
 
     displayCalculated();
 }
 
 function displayCalculated() {
 
-    // document.querySelector('#coalsd').innerText = pc.coalsd;
-    // document.querySelector('#coalsm').innerText = pc.coalsm;
-    // document.querySelector('#totalcoal').innerText = pc.totalcoal;
-    // document.querySelector('#obsd').innerText = pc.obsd;
-    // document.querySelector('#obdl').innerText = pc.obdl;
-    // document.querySelector('#dlreh').innerText = pc.dlreh;
-    // document.querySelector('#deptob').innerText = pc.deptob;
-    // document.querySelector('#outob').innerText = pc.outob;
-    // document.querySelector('#totalob').innerText = pc.totalob;
-    // document.querySelector('#silog8').innerText = pc.silog8;
-    // document.querySelector('#silog10').innerText = pc.silog10;
-    // document.querySelector('#totaldesp').innerText = pc.totaldesp;
-    // document.querySelector('#netdesp').innerText = pc.netdesp;
-
-
-    // document.querySelector('#dlah').innerText = hc.dlah;
-    // document.querySelector('#dlwh').innerText = hc.dlwh;
-    // document.querySelector('#shvah').innerText = hc.shvah;
-    // document.querySelector('#shvwh').innerText = hc.shvwh;
-    // document.querySelector('#d85ah').innerText = hc.d85ah;
-    // document.querySelector('#d85wh').innerText = hc.d85wh;
-    // document.querySelector('#d120ah').innerText = hc.d120ah;
-    // document.querySelector('#d120wh').innerText = hc.d120wh;
-    // document.querySelector('#dumperah').innerText = hc.dumperah;
-    // document.querySelector('#dumperwh').innerText = hc.dumperwh;
-
-    for (x of Object.keys(pc)) {
-        document.querySelector('#' + x).innerText = pc[x];
+    for (x of Object.keys(pcv)) {
+        document.querySelector('#' + x).innerText = pcv[x];
     }
 
-    for (x of Object.keys(hc)) {
-        document.querySelector('#' + x).innerText = hc[x];
+    for (x of Object.keys(hcv)) {
+        document.querySelector('#' + x).innerText = hcv[x];
     }
 }
 
